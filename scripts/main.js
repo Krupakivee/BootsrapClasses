a70bd8f9ea4e3df541d3c47c13ba00a6d92bbabc
Nishi <nishis.kiveesoftech@gmail.com>
2020-12-02 18:10:42 +0530

    changes 2/12/2020

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    var counter = 0;
     var quantitiy = 0;
     $('.quantity-right-plus').click(function (e) {
 
@@ -31,21 +31,85 @@ $(document).ready(function () {
         }
     });
 
-    $("#btncontact").click(function () {
-        var phoneNumber = $("#cnumber").val();
-        var name = $("#name").val();
-        var phone = /^\d{10}$/;
-        //   var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
-        if (name.length < 3 || name.length > 20) {
-            $('#name_error').html("length between 2-20");
-            $("#name").css("border", "3px solid red");
+
+    $("#contactfrom").validate({
+        rules: {
+            'name': {
+                required: true,
+                minlength: 2
+            },
+            'email': {
+                required: true,
+                email: true,
+            },
+            'phone': {
+                required: true,
+            },
+            'message': {
+                required: true,
+                minlength: 5
+            },
+
+        },
+        messages: {
+            'name': "The name is Required",
+            'email': "Please enter Email",
+            'Phone': "Please enter a Password",
+            'message': "The message is required",
         }
-        else if (!phoneNumber.match(phone)) {
-            $('#phone_error').html("* Enter valid 10 digit number like this 9876543210.");
-            $("#cnumber").css("border", "3px solid red");
-        }
-        else {
-            alert("thank you for submitting");
+    });
+    $(".btn-danger").click(function () {
+        counter++;
+        $("#counter").text(counter);
+    });
+    var itemCount = 0;
+    var priceTotal = 0;
+    // Add Item to Cart
+    $('.add').click(function () {
+        itemCount++;
+
+        $('#itemCount').text(itemCount).css('display', 'block');
+        $(this).siblings().clone().appendTo('#cartItems').append('<button class="removeItem">Remove Item</button>');
+
+        // Calculate Total Price
+        var price = parseInt($(this).siblings().find('.price').text());
+        priceTotal += price;
+        $('#cartTotal').text("Total: $" + priceTotal);
+    });
+
+
+
+    // Hide and Show Cart Items
+    $('.openCloseCart').click(function () {
+        $('#shoppingCart').toggle();
+    });
+
+
+    // Empty Cart
+    $('#emptyCart').click(function () {
+        itemCount = 0;
+        priceTotal = 0;
+
+        $('#itemCount').css('display', 'none');
+        $('#cartItems').text('');
+        $('#cartTotal').text("Total: $" + priceTotal);
+    });
+
+
+
+    // Remove Item From Cart
+    $('#shoppingCart').on('click', '.removeItem', function () {
+        $(this).parent().remove();
+        itemCount--;
+        $('#itemCount').text(itemCount);
+
+        // Remove Cost of Deleted Item from Total Price
+        var price = parseInt($(this).siblings().find('.price').text());
+        priceTotal -= price;
+        $('#cartTotal').text("Total: $" + priceTotal);
+
+        if (itemCount == 0) {
+            $('#itemCount').css('display', 'none');
         }
     });
 });
